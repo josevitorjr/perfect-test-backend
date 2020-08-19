26078c767cda4aea0a66a51a696a388d608cd517
José Vitor <josevitorrjunior@outlook.com>
2020-08-19 10:04:16 -0300

    Feature: Add validation to create products

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Http\Requests\ProductRequest;
 use App\Models\ProductModel;
 
 class ProductController extends Controller
@@ -38,7 +38,7 @@ class ProductController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(ProductRequest $request)
     {
         $cad= $this->objProduct->create([
             'nome'=>$request->name,
@@ -79,7 +79,7 @@ class ProductController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(ProductRequest $request, $id)
     {
         //
     }
--- resources/views/crud_products.blade.php
+++ resources/views/crud_products.blade.php
@@ -4,19 +4,29 @@
     <h1>Adicionar / Editar Produto</h1>
     <div class='card'>
         <div class='card-body'>
+        @if(isset($errors) && count($errors)>0)
+            <div class="alert alert-danger alert-dismissible fade show">
+                @foreach($errors->all() as $erro)
+                {{$erro}}<br>
+                @endforeach
+                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+        @endif
             <form method="post" action="{{url('products')}}">
                 @csrf
                 <div class="form-group">
                     <label for="name">Nome do produto</label>
-                    <input type="text" class="form-control " id="name" name="name">
+                    <input type="text" class="form-control " id="name" name="name" required>
                 </div>
                 <div class="form-group">
                     <label for="description">Descrição</label>
-                    <textarea type="text" rows='5' class="form-control" id="description" name="description"></textarea>
+                    <textarea type="text" rows='5' class="form-control" id="description" name="description" required></textarea>
                 </div>
                 <div class="form-group">
                     <label for="price">Preço</label>
-                    <input type="text" class="form-control" id="price" name="price" placeholder="100,00 ou maior">
+                    <input type="text" class="form-control" id="price" name="price" placeholder="100,00 ou maior" required>
                 </div>
                 <button type="submit" class="btn btn-primary">Salvar</button>
             </form>
